68fe8e2a934a648688b774bebd29689371738c23
Jefferson Alves <jefff9910@outlook.com>
2022-03-13 18:35:36 -0300

    Layou em desenvolvimento

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'studio-bagaz';
+  activeTitle: boolean = true
+  changeImg: number = 2
+
+  
+  ngOnInit(): void {
+    setInterval(() => {
+      switch(this.changeImg) {
+        case 1:
+          this.changeImg = 2
+          break
+        case 2:
+          this.changeImg = 3
+          break
+        case 3:
+          this.changeImg = 1
+          break
+      }
+      
+    }, 2000);
+   
+  }
+
+
 }
